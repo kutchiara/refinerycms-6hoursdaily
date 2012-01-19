3950ce1b380dc55affdc15b4584695bd03c1d4f1
jerry <jerrypascua@gmail.com>
2012-01-19 22:12:03 +0800

    "initial commit "

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanyamariano::Application.config.secret_token = '19d969614c006ea0543cd3fbf8c12651ba52b7fa221e1df3e1734f4a2a2375447e9fb73aeaac29434ffa424647fca1d92324e95babbd0afee1292cd3001832b7'
